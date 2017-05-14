9e03196e96612128828f27c449d585f91b0a243e
ChangJoo Park <pcjpcj2@gmail.com>
2017-05-14 22:17:43 +0900

    Add JaSerializer

--- mix.exs
+++ mix.exs
@@ -30,12 +30,15 @@ defmodule PhoenixJsonApi.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:phoenix, "~> 1.2.1"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_ecto, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
+      {:ja_serializer, "~> 0.12.0"}
+   ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
